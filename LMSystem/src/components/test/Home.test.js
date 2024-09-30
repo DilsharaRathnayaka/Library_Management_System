5e0f3d01eaffe355a80c69c0beb7e447273e010b
Kalpana_Perera <118042669+KalpanaPerera@users.noreply.github.com>
2024-09-30 21:12:16 +0530

    delete

--- LMSystem/src/components/test/Home.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Home from './Home';
-
-test('renders homepage', () => {
-    render(<Home />);
-    expect(screen.getByText(/welcome to the e-library/i)).toBeInTheDocument();
-});
